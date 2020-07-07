d4700f5b6a6943adb95d5eb34b08fb5a6f32d3cc
Mateus Bezerra <mateusbzerra@gmail.com>
2020-07-06 22:35:53 -0300

    feat: implements customers module

--- src/modules/customers/infra/http/controller/CustomersController.ts
+++ src/modules/customers/infra/http/controller/CustomersController.ts
@@ -3,9 +3,19 @@ import { Request, Response } from 'express';
 import CreateCustomerService from '@modules/customers/services/CreateCustomerService';
 
 import { container } from 'tsyringe';
+import AppError from '@shared/errors/AppError';
 
 export default class CustomersController {
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { name, email } = request.body;
+
+    const createCustomerService = container.resolve(CreateCustomerService);
+
+    if (!name && !email) {
+      throw new AppError('Missing parameters', 422);
+    }
+
+    const customer = await createCustomerService.execute({ name, email });
+    return response.json(customer);
   }
 }
--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,23 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const checkIfEmailAlreadyExists = this.customersRepository.findByEmail(
+      email,
+    );
+
+    if (checkIfEmailAlreadyExists) {
+      throw new AppError('Email already registered');
+    }
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
