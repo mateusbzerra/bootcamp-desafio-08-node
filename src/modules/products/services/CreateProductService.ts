2843b65b05d81d2d1a7d18fafda2aa51a621a710
Mateus Bezerra <mateusbzerra@gmail.com>
2020-07-11 12:08:53 -0300

    feat(products): add create product service

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,24 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const checkIfNameAlreadyExists = await this.productsRepository.findByName(
+      name,
+    );
+    if (checkIfNameAlreadyExists) {
+      throw new AppError('Product already registed');
+    }
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+    return product;
   }
 }
 
